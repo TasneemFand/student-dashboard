93d91a46ac89d998fd59cba7ed269c7f4c1e4ced
Tasneem Fandakli <tasnimtepe@gmail.com>
2024-06-30 00:53:07 +0300

    add responsive for auth

--- src/pages/auth/login/page.tsx
+++ src/pages/auth/login/page.tsx
@@ -1,28 +1,35 @@
-import { Box } from "@mui/material";
+import { Box, Theme, useMediaQuery } from "@mui/material";
 import { LangSelect } from "../../../components/lang-select";
 import { LoginForm } from "./components/login-form";
 
 export const Login = () => {
+  const isMobile = useMediaQuery<Theme>((theme) =>
+    theme.breakpoints.down("sm")
+  );
+
   return (
     <Box sx={{ display: "flex", height: "100%", gap: 3 }}>
-      <Box
-        sx={{
-          flex: 1,
-          display: "flex",
-          flexDirection: "column",
-          padding: 5,
-          paddingX: 6,
-          gap: "100px",
-        }}
-      >
-        <LangSelect />
-        <img
-          loading="lazy"
-          src={"/dashboard/students/assests/login/login1.svg"}
-          alt="login"
-          width={"100%"}
-        />
-      </Box>
+      {isMobile ? null : (
+        <Box
+          sx={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            padding: 5,
+            paddingX: 6,
+            gap: "100px",
+          }}
+        >
+          <LangSelect />
+          <img
+            loading="lazy"
+            src={"/dashboard/students/assests/login/login1.svg"}
+            alt="login"
+            width={"100%"}
+          />
+        </Box>
+      )}
+
       <Box
         sx={{
           flex: 2,
@@ -31,12 +38,26 @@ export const Login = () => {
           backgroundSize: "cover",
           backgroundPositionY: "center",
           display: "flex",
-          justifyContent: "center",
-          alignItems: "center"
+          justifyContent: isMobile ? "space-evenly" : "center",
+          alignItems: "center",
+          flexDirection: isMobile ? "column" : "row",
         }}
       >
-        <Box sx={{ backgroundColor: "white",paddingY: 5, paddingX: 5, borderRadius: "6px", maxWidth: "500px"}}>
-           <img src={"/dashboard/students/assests/login/loginText.svg"} alt="login" style={{ marginBottom: "24px", width: "80px"}}/>
+        {isMobile ? <LangSelect /> : null}
+        <Box
+          sx={{
+            backgroundColor: "white",
+            paddingY: 5,
+            paddingX: 5,
+            borderRadius: "6px",
+            maxWidth: "500px",
+          }}
+        >
+          <img
+            src={"/dashboard/students/assests/login/loginText.svg"}
+            alt="login"
+            style={{ marginBottom: "24px", width: "80px" }}
+          />
           <LoginForm />
         </Box>
       </Box>
